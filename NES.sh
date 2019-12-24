7af69da9059c725601730e0430632eb80cd38ca0
newbie-error <56509248+newbie-error@users.noreply.github.com>
2019-12-24 10:03:32 +0700

    Add files via upload

---
+++ NES.sh
@@ -0,0 +1,359 @@
+#!/system/xbin/bash
+# Bersihkan Layar
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+###################################################
+# Channel Dark Ice
+###################################################
+echo
+echo
+echo
+echo
+sleep 1
+echo  $red"SUBSCRIBE CHANNEL DARK ICE"
+sleep 3
+xdg-open 'https://www.youtube.com/channel/UC65i9obO8PGARlIXsb274MA'
+clear
+echo
+echo  $blue"╔══════════════════════════════════════════════════════════════════╗"
+echo  $purple"║ [ • ] TEAM      : NEWBIE ERROR SYSTEM                            ║"
+echo  $blue"║ [ • ] GMAIL     : darkice2311@gmail.com                          ║"
+echo  $purple"║ [ • ] THANKS TO :DARK ICE                                        ║"
+echo  $blue"║ [ • ] :::::::::::::::::::::YOUTUBE CHANNEL:::::::::::::::::::::  ║"
+echo  $purple"║ [ • ] https://www.youtube.com/channel/UC65i9obO8PGARlIXsb274MA   ║"
+echo  $blue"╚══════════════════════════════════════════════════════════════════╝"
+echo  $yellow"╔═══════════════════════════════════════════╗"
+echo  $yellow"║ .:::::::::::::::::TOOLS:::::::::::::::::. ║"
+echo  $yellow"║ .::::::::::NEWBIE ERROR SYSTEM::::::::::. ║"
+echo  $yellow"╚═══════════════════════════════════════════╝"
+sleep 1
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $white" [INSTALL] UNTUK MENGINSTALL PAKET"
+echo  $purple" ═══════════════════════════════════════════════"
+echo  $white" [00] EXIT"
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $white" [01] VIRTEXT [UP DC] NEWBIE ERROR SYSTEM"
+echo  $purple" ═══════════════════════════════════════════════"
+echo  $white" [02] SPAM SMS BY zlucifer"
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $white" [03] VIRTEXT BY  ⃘⃤ᴋɪʀᴀꙮ≈⊰0⊱"
+echo  $purple" ═══════════════════════════════════════════════"
+echo  $white" [04] DOWNLOAD APK SMS GRATIS"
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $white" [05] CEK IP"
+echo  $purple" ═══════════════════════════════════════════════"
+echo  $white" [06] TEMBAK PAKET"
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $white" [07] DDOS ATTACK (HAMMER)"
+echo  $purple" ═══════════════════════════════════════════════"
+echo  $white" [08] DOWNLOAD BUKU SAKTI HACKER"
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $white" [09] INSTALL TOOLS TU4N BADUT(300TOOLS LEBIH)"
+echo  $purple" ═══════════════════════════════════════════════"
+echo  $white" [10] TOOLS SERBA GUNA"
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $white" [11] CREATE VIRUS"
+echo  $purple" ═══════════════════════════════════════════════"
+echo  $white" [12] SEND VIRUS TROJAN"
+echo  $blue" ═══════════════════════════════════════════════"
+echo  $white" [13] SPECIAL APK SPAM WA UNLIMITED"
+echo  $purple" ═══════════════════════════════════════════════"
+echo  $green""
+read -p "[Dark Ice]> " bro;
+
+if [ $bro = INSTALL ] || [ $bro = install ]
+then
+clear
+echo
+echo
+sleep 1
+toilet PROSES | lolcat
+toilet INSTALLASI | lolcat
+sleep 3
+pkg install nmap
+pkg install python
+pkg install python2
+pkg install python3
+pip2 install requests
+pip2 install bs4
+pkg install lolcat
+pkg install toilet
+pip2 install mechanize
+pkg install cl
+pkg install vim
+gem install lolcat
+pkg install cowsay
+pkg install neofetch
+clear
+fi
+
+
+
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+clear
+echo
+echo  $red" TERIMA KASIH TELAH MENGGUNAKAN TOOLS INI"
+sleep 3
+xdg-open 'https://www.youtube.com/channel/UC65i9obO8PGARlIXsb274MA'
+clear
+exit
+fi
+
+if [ $bro = 1 ] || [ $bro = 01 ]
+then
+clear
+echo  $red" CREATOR : DARK ICE"
+sleep 1
+echo  $red" MOHON TUNGGU SEBENTAR "
+sleep 3
+echo  $red"loading......"
+sleep 2
+echo  $red"ANDA AKAN DI ARAHKAN KE LINK DOWNLOAD NYA"
+sleep 5
+xdg-open 'https://hokiciki.org/82mXr'
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+if [ $bro = 2 ] || [ $bro = 02 ]
+then
+clear
+echo  $red" SPAM SMS BY zlucifer "
+sleep 1
+echo  $red" tunggu sebentar  "
+sleep 1
+echo  $red" loading..... "
+sleep 3
+pkg update && pkg upgrade
+pkg install git
+pkg install curl
+git clone https://github.com/zlucifer/troll_project
+cd troll_project
+chmod +×*
+ls
+bash troll.sh
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+if [ $bro = 3 ] || [ $bro = 03 ]
+then
+clear
+echo  $red"MOHON TUNGGU SEBENTAR"
+sleep 3
+echo  $red"ANDA AKAN DI ARAHKAN KE LINK DOWNLOAD"
+sleep 5
+xdg-open 'https://hokiciki.org/tHI2V'
+
+sleep 5
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+if [ $bro = 4 ] || [ $bro = 04 ]
+then
+clear
+echo  $green"SUBSCRIBE DLU CHANNEL DARK ICE"
+sleep 1
+echo  $green"GUE KASIH WAKTU 10 DETIK"
+xdg-open 'https://www.youtube.com/channel/UC65i9obO8PGARlIXsb274MA'
+sleep 10
+echo  $red"MOHON TUNGGU SEBENTAR"
+sleep 3
+echo  $red"ANDA AKAN DI ARAHKAN KE LINK DOWNLOAD"
+sleep 1
+echo  $red"Loading......."
+sleep 3
+xdg-open 'https://hokiciki.org/ZiN3y'
+sleep 5
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+
+
+
+if [ $bro = 5 ] || [ $bro = 05 ]
+then
+clear
+echo  $red" CEK IP "
+sleep 1
+echo  $red"TUNGGU SEBENTAR"
+sleep 1
+echo  $red"SUBSCRIBE CHANNEL DARK ICE"
+sleep 3
+xdg-open 'https://www.youtube.com/channel/UC65i9obO8PGARlIXsb274MA'
+pkg update
+pkg upgrade
+pkg install net-tools
+ifconfig
+
+sleep 3
+fi
+
+
+
+
+if [ $bro = 6 ] || [ $bro = 06 ]
+then
+clear
+echo
+sleep 1
+echo  $red" MOHON TUNGGU SEBENTAR "
+sleep 3
+echo  $red"loading......"
+sleep 2
+git clone https://github.com/bintang73/tembaktembakan
+cd tembaktembakan
+python dor.py
+
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+
+
+
+if [ $bro = 7 ] || [ $bro = 07 ]
+then
+clear
+echo
+sleep 1
+echo  $red" MOHON TUNGGU SEBENTAR "
+sleep 3
+echo  $red"loading......"
+sleep 2
+git clone https://github.com/cyweb/hammer
+cd hammer
+chmod +x hammer.py
+python hammer.py
+
+
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+
+
+
+
+if [ $bro = 8 ] || [ $bro = 08 ]
+then
+clear
+echo  $red" CREATOR : DARK ICE"
+sleep 1
+echo  $red" MOHON TUNGGU SEBENTAR "
+sleep 3
+echo  $red"loading......"
+sleep 2
+xdg-open 'https://transmediakreatif.com/syR0V'
+
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+
+if [ $bro = 9 ] || [ $bro = 09 ]
+then
+clear
+echo
+sleep 1
+echo  $red" MOHON TUNGGU SEBENTAR "
+sleep 3
+echo  $red"loading......"
+sleep 2
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+chmod +x TUANB4DUT.sh
+sh TUANB4DUT.sh
+
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+
+if [ $bro = 10 ] || [ $bro = 010 ]
+then
+clear
+echo
+sleep 1
+echo  $red" MOHON TUNGGU SEBENTAR "
+sleep 3
+echo  $red"loading......"
+sleep 2
+git clone https://github.com/muhammadfathul/FBH
+cd FBH
+chmod +x*
+bash FBH
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+if [ $bro = 11 ] || [ $bro = 011 ]
+then
+clear
+echo
+sleep 1
+echo  $red" MOHON TUNGGU SEBENTAR "
+sleep 3
+echo  $red"loading......"
+sleep 2
+git clone https://github.com/LOoLzeC/Evil-create-framework
+cd Evil-create-framework
+python2 vcrt.py
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+
+if [ $bro = 12 ] || [ $bro = 012 ]
+then
+clear
+echo
+sleep 1
+echo  $red" MOHON TUNGGU SEBENTAR "
+sleep 3
+echo  $red"loading......"
+sleep 2
+git clone https://github.com/MrTamfanX/MrDdos
+cd MrDdos
+chmod +x*
+sh Tamfan-Ddos.sh
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
+
+
+if [ $bro = 13 ] || [ $bro = 013 ]
+then
+clear
+echo
+sleep 1
+echo  $red" MOHON TUNGGU SEBENTAR "
+sleep 3
+echo  $red"loading......"
+sleep 3
+xdg-open 'http://www.mediafire.com/file/02cg15j48zi6s1r/Labalabi_3.0_JalanTikus.apk/file'
+clear
+toilet TERIMA | lolcat
+toilet KASIH | lolcat
+fi
